80cb0c8a8c0cf001d8d36feaabd808a7059f477d
muhammadqahfi <qahfilayau1745@gmail.com>
2020-04-14 11:49:36 +0800

    Memanggil method dari Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa =========================");
+  System.out.println("NPM : 18631114");
+  System.out.println("Nama : Muhammad qahfi");
+  System.out.println("======================================");
+ }
+}
+  
---
+++ model/MataKuliah.java
@@ -0,0 +1,12 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mata Kuliah =========================");
+  System.out.println("kode : T1F3402");
+  System.out.println("Nama : pemrograman Berbasis objek 1");
+  System.out.println("======================================");
+ }
+}
+  
